aafc80483b1fe394bf02b595c64fffeceaa9284d
Anton Karmanov <bergertroll@insiberia.net>
2020-09-02 23:38:17 +0500

    Implement semicolons check

--- inc/clusterer.hpp
+++ inc/clusterer.hpp
@@ -2,13 +2,45 @@
 
 #include <algorithm>
 #include <dlib/clustering.h>
+#include <iostream>
 #include <optional>
 #include <sstream>
+#include <string>
 #include <vector>
 
 namespace otus {
   constexpr int dataSize = 7;
   using DataType = dlib::matrix<float, dataSize, 1>;
+
+  template <typename T>
+  class InputValidator {
+  public:
+    class InvalidToken: public std::runtime_error {
+    public:
+      InvalidToken(std::string const & message): std::runtime_error(message) { }
+    };
+
+    InputValidator(T const & value): expected(value) { }
+  private:
+    T expected;
+
+    template <typename U>
+    friend std::istream & operator>>(std::istream & is, InputValidator<U> & val);
+  };
+
+  template <typename T>
+  std::istream & operator>>(std::istream & is, InputValidator<T> & val) {
+    char buf;
+    is >> buf;
+    if (buf != val.expected) {
+      std::stringstream ss { };
+      ss
+        << "expected \"" << val.expected << '"'
+        << ", but \"" << buf << "\" given";
+      throw typename InputValidator<T>::InvalidToken(ss.str());
+    }
+    return is;
+  }
 
   class Flat {
   public:
@@ -20,14 +52,20 @@ namespace otus {
 
       std::stringstream ss { description };
 
-      /// TODO: Check input.
-      char semiColon;
+      InputValidator<char> semiColon { ';' };
 
-      ss
-        >> latitude >> semiColon >> longitude >> semiColon
-        >> rooms >> semiColon >> price >> semiColon
-        >> square >> semiColon >> kitchen >> semiColon
-        >> floor >> semiColon >> storeys >> semiColon;
+      try {
+        ss
+          >> latitude >> semiColon >> longitude >> semiColon
+          >> rooms >> semiColon >> price >> semiColon
+          >> square >> semiColon >> kitchen >> semiColon
+          >> floor >> semiColon >> storeys;
+      }
+      catch (InputValidator<char>::InvalidToken const &e) {
+        std::cerr
+          << "Invalid input \"" << ss.str() << "\": "
+          <<  e.what() << std::endl;
+      }
 
       bool isNotBoundary { false };
 
