958cc1d3fd2204a3276cea0b6d5fc57406e877bc
Anton Karmanov <bergertroll@insiberia.net>
2020-09-05 20:25:59 +0500

    Update clusterer.hpp
    Split Flat to individual header.
    
    Make data ref const in c'tor of Clusterer.

--- inc/clusterer.hpp
+++ inc/clusterer.hpp
@@ -3,99 +3,21 @@
 #include <algorithm>
 #include <dlib/clustering.h>
 #include <iostream>
-#include <optional>
-#include <sstream>
 #include <string>
 #include <vector>
 
+#include "flat.hpp"
+
 namespace otus {
-  constexpr int dataSize = 7;
-  using DataType = dlib::matrix<float, dataSize, 1>;
-
-  class InvalidToken: public std::runtime_error {
-  public:
-    InvalidToken(std::string const & message):
-    std::runtime_error(message) { }
-  };
-
-  template <typename T>
-  class InputValidator {
-  public:
-    class ConcreteInvalidToken: public InvalidToken {
-    public:
-      ConcreteInvalidToken(std::string const & message):
-      InvalidToken(message) { }
-    };
-
-    InputValidator(T const & value): expected(value) { }
-  private:
-    T expected;
-
-    template <typename U>
-    friend std::istream & operator>>(std::istream & is, InputValidator<U> & val);
-  };
-
-  template <typename T>
-  std::istream & operator>>(std::istream & is, InputValidator<T> & val) {
-    char buf;
-    is >> buf;
-    if (buf != val.expected) {
-      std::stringstream ss { };
-      ss
-        << "expected \"" << val.expected << '"'
-        << ", but \"" << buf << "\" given";
-      throw typename InputValidator<T>::ConcreteInvalidToken(ss.str());
-    }
-    return is;
-  }
-
-  class Flat {
-  public:
-    using Cluster = std::optional<unsigned long>;
-
-    Flat(std::string const &description) {
-      float latitude, longitude, price, square, kitchen;
-      int rooms, storeys, floor;
-
-      std::stringstream ss { description };
-
-      InputValidator<char> semiColon { ';' };
-
-      ss
-        >> latitude >> semiColon >> longitude >> semiColon
-        >> rooms >> semiColon >> price >> semiColon
-        >> square >> semiColon >> kitchen >> semiColon
-        >> floor >> semiColon >> storeys;
-
-      bool isNotBoundary { false };
-
-      if (floor > 1 && floor < storeys) {
-        isNotBoundary = true;
-      }
-
-      data = {
-        latitude, longitude,
-        static_cast<float>(rooms),
-        price, square, kitchen,
-        static_cast<float>(isNotBoundary) };
-    }
-
-    operator DataType() const { return data; }
-
-  private:
-    DataType data;;
-  };
-
   class Clusterer {
   public:
     Clusterer(
-        std::vector<Flat> & data,
+        std::vector<Flat> const & data,
         int numberOfClusters,
         float gamma=0.1,
         float accuracy=0.01):
     kMeans(dlib::kcentroid(KernelType(gamma), accuracy)) {
       this->data.reserve(data.size());
-      // TODO Copy data.
       std::transform(
           data.begin(),
           data.end(),
